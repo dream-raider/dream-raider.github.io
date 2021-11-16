6bb254fe22cabc04ac88a53833cb062aad563550
dream-raider <joshierika@gmail.com>
2021-11-16 16:58:59 -0600

    js validated

--- english-dictionary/dict.js
+++ english-dictionary/dict.js
@@ -8,29 +8,29 @@ $(document).ready(function () {
 
         $word = $("#word").val();
         if ($("#word").val().length === 0) {
-            alert('Enter value to search')
+            alert("Enter value to search");
         } else {
-            $('#loader').show();
+            $("#loader").show();
         }
         $.ajax({
-            url: "http://localhost:2021/search",
-            type: 'POST',
-            data: { word: $word },
-
-            success: successFunction,
-            complete: function () { // Set our complete callback, adding the .hidden class and hiding the spinner.
-                $('#loader').hide();
+            complete: function () {
+                // Set our complete callback, adding the .hidden class and hiding the spinner.
+                $("#loader").hide();
             },
+            data: { word: $word },
+            success: successFunction,
+            type: "POST",
+            url: "http://localhost:2021/search"
         });
     }
 
     function successFunction(data) {
         parsedData = JSON.parse(data);
         if (parsedData.length === 0) {
-            alert("Enter valid keyword to search")
+            alert("Enter valid keyword to search");
         } else {
-            for (var i = 0; i < parsedData.length; i++) {
-                $("ul").append(`<li>${i + 1} (${parsedData[i].wordtype}) :: ${parsedData[i].definition}</li>`)
+            for (let i = 0; i < parsedData.length; i++) {
+                $("ul").append("<li>${i + 1} (${parsedData[i].wordtype}) :: ${parsedData[i].definition}</li>");
             }
         }
 
